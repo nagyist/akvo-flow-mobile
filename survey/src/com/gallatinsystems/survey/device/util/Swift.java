c7f4df5ce284d3ca80dcf829bd71dd5e48342d46
Inigo Lopez de Heredia <inigo@akvo.org>
2013-09-17 11:04:32 +0100

    Issue #27 - Switch to CloudVPS objectstore
    * As recommended by CloudVPS, use HTTP Basic Auth (on top of TLS)

--- survey/src/com/gallatinsystems/survey/device/util/Swift.java
+++ survey/src/com/gallatinsystems/survey/device/util/Swift.java
@@ -25,30 +25,18 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 import org.apache.http.HttpStatus;
 
-import android.text.TextUtils;
 import android.util.Log;
 
-import com.turbomanage.httpclient.HttpResponse;
-import com.turbomanage.httpclient.android.AndroidHttpClient;
-
 /**
- * OpenStack/Swift uploader. This version uses Swift v1.0, with Token
- * based authentication.
- * <br>
- * TODO: 
+ * OpenStack/Swift uploader. This version uses HTTP Basic Authentication <br>
+ * TODO:
  * <ul>
- * <li>Cache the token and storage url</li>
- * <li>Upgrade API version depending on the backend</li>
  * <li>Add MIME type to objects</li>
  * <li>Discuss container security options (public/private)</li>
  * </ul>
- *
  */
 public class Swift {
 
@@ -58,75 +46,52 @@ public class Swift {
     public interface UploadListener {
         public void uploadProgress(long bytesUploaded, long totalBytes);
     }
-    
+
     private static final String TAG = Swift.class.getSimpleName();
     private static final int BUFFER_SIZE = 8192;
 
     private String mApiUrl;
     private String mUsername;
-    private String mApiKey;
-    private String mStorageUrl;
-    private String mToken;
-    
-    public Swift(String apiUrl, String username, String apiKey) {
+    private String mPassword;
+
+    public Swift(String apiUrl, String username, String password) {
         mApiUrl = apiUrl;
         mUsername = username;
-        mApiKey = apiKey;
+        mPassword = password;
     }
-    
-    public boolean uploadFile(String container, String name, File file, 
-            UploadListener listener) {
+
+    public boolean uploadFile(String container, String name, File file,
+            UploadListener listener){
+        Log.i(TAG, "uploading file: " + name);
         try {
-            boolean reauthenticate = true;
-            if (TextUtils.isEmpty(mToken)) {
-                reauthenticate = false;
-                authenticate();
-            }
-            
-            return uploadFile(container, name, file, reauthenticate, listener);
-        } catch (ApiException e) {
-            Log.e(TAG, e.getMessage());
-            return false;
+            return put(container, name, file, listener);
         } catch (IOException e) {
             Log.e(TAG, e.getMessage());
             return false;
         }
     }
-    
-    private boolean uploadFile(String container, String name, File file, boolean reauthenticate, 
-            UploadListener listener) throws ApiException, IOException {
-        Log.i(TAG, "uploading file: " + name);
-        try {
-            return put(container, name, file, listener);
-        } catch (UnauthorizedException e) {
-            if (reauthenticate) {
-                authenticate();
-                return uploadFile(container, name, file, false, listener);
-            }
-            return false;
-        }
-    }
-    
-    private boolean put(String container, String name, File file, UploadListener listener) 
-            throws UnauthorizedException, IOException {
+
+    private boolean put(String container, String name, File file, UploadListener listener)
+            throws IOException {
         InputStream in = null;
         OutputStream out = null;
-        HttpURLConnection conn = null; 
-        
+        HttpURLConnection conn = null;
+        boolean ok = false;
+
         try {
-            URL url = new URL(mStorageUrl + "/" + container + "/" + name);
-            conn = (HttpURLConnection)url.openConnection();
+            URL url = new URL(mApiUrl + "/" + container + "/" + name);
+            conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
             conn.setRequestMethod("PUT");
-            conn.setRequestProperty(Header.AUTH_TOKEN, mToken);
+            conn.setRequestProperty(Header.AUTH, getAuthHeader());
             conn.setRequestProperty(Header.ETAG, FileUtil.getMD5Checksum(file));
-            
+
             in = new BufferedInputStream(new FileInputStream(file));
             out = new BufferedOutputStream(conn.getOutputStream());
-            
+
             final long totalBytes = file.length();
             long bytesWritten = 0;
-            
+
             byte[] b = new byte[BUFFER_SIZE];
             int read;
             while ((read = in.read(b)) != -1) {
@@ -137,7 +102,7 @@ public class Swift {
                 }
             }
             out.flush();
-            
+
             int status = 0;
             try {
                 status = conn.getResponseCode();
@@ -145,19 +110,18 @@ public class Swift {
                 // HttpUrlConnection will throw an IOException if any 4XX
                 // response is sent. If we request the status again, this
                 // time the internal status will be properly set, and we'll be
-                // able to retrieve it. What mastermind designed this?
+                // able to retrieve it.
                 status = conn.getResponseCode();
             }
-            if (status == HttpStatus.SC_UNAUTHORIZED) {
-                throw new UnauthorizedException("401 - Unauthorized");
-            } else if (status != HttpStatus.SC_CREATED) {
-                throw new ApiException("Status Code: " + status + 
-                        ". Expected: 201 - Created");
+            ok = (status == HttpStatus.SC_CREATED);
+            if (!ok) {
+                Log.e(TAG, "Status Code: " + status + ". Expected: 201 - Created");
             }
-            
-            return true;
+
+            return ok;
         } finally {
-            if (conn != null) conn.disconnect();
+            if (conn != null)
+                conn.disconnect();
             if (in != null) {
                 try {
                     in.close();
@@ -170,79 +134,15 @@ public class Swift {
             }
         }
     }
-    
-    /**
-     * Token based authentication.
-     * TODO: cache the token & storage url
-     * 
-     * @throws ApiException
-     */
-    private void authenticate() throws ApiException {
-        AndroidHttpClient httpClient = new AndroidHttpClient(mApiUrl);
-        httpClient.addHeader(Header.AUTH_USER, mUsername);
-        httpClient.addHeader(Header.AUTH_KEY, mApiKey);
-        
-        HttpResponse response = httpClient.get(Api.AUTH, null);
-        
-        if (response != null && response.getStatus() == HttpStatus.SC_OK) {
-            mToken = getHeader(response.getHeaders(), Header.AUTH_TOKEN);
-            mStorageUrl = getHeader(response.getHeaders(), Header.STORAGE_URL);
-        } else {
-            StringBuilder message = new StringBuilder("Could not authenticate with Swift. ");
-            if (response != null) {
-                message.append("Status Code: ").append(response.getStatus());
-            }
-            else {
-                message.append("HttpResponse is null");
-            }
-            throw new ApiException(message.toString());
-        }
-    }
-    
-    private String getHeader(Map<String, List<String>> headers, String name) {
-        if (headers != null) {
-            List<String> headerValues = headers.get(name);
-            if (headerValues == null) {
-                // Issue #13 -
-                // https://github.com/akvo/akvo-flow-mobile/issues/13
-                // Prior to Gingerbread, HttpUrlConnection converted
-                // all response headers to lower case. This is a workaround
-                // to ensure we cover those situations as well
-                final String lowercaseName = name.toLowerCase(Locale.ENGLISH);
-                headerValues = headers.get(lowercaseName);
-            }
+	
+	private String getAuthHeader() {
+		final String userPassword = mUsername + ":" + mPassword;
+        final String auth = Base64.encodeBytes(userPassword.getBytes());
+		return "Basic " + auth;
+	}
 
-            if (headerValues != null && headerValues.size() > 0) {
-                return headerValues.get(0);
-            }
-        }
-
-        return null;
-    }
-    
-    @SuppressWarnings("serial")
-    class ApiException extends RuntimeException {
-        public ApiException(String message) {
-            super(message);
-        }
-    }
-    
-    @SuppressWarnings("serial")
-    class UnauthorizedException extends ApiException {
-        public UnauthorizedException(String message) {
-            super(message);
-        }
-    }
-    
-    interface Api {
-        static final String AUTH = "/auth/v1.0";
-    }
-    
     interface Header {
-        static final String AUTH_USER   = "X-Auth-User";
-        static final String AUTH_KEY    = "X-Auth-Key";
-        static final String AUTH_TOKEN  = "X-Auth-Token";
-        static final String STORAGE_URL = "X-Storage-Url";
-        static final String ETAG        = "ETag";
+        static final String AUTH = "Authorization";
+        static final String ETAG = "ETag";
     }
 }
