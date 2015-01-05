be23b995c03b9213a13aee61ee6f04a62d9573d9
Inigo Lopez de Heredia <inigo@akvo.org>
2015-01-05 11:39:26 +0000

    [#238] Basic osmdroid integration
    * No markers support, only map display

--- app/src/main/java/org/akvo/flow/ui/fragment/MapFragment.java
+++ app/src/main/java/org/akvo/flow/ui/fragment/MapFragment.java
@@ -24,6 +24,7 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.Loader;
 import android.util.Log;
@@ -39,24 +40,13 @@ import org.akvo.flow.async.loader.SurveyedLocaleLoader;
 import org.akvo.flow.dao.SurveyDbAdapter;
 import org.akvo.flow.domain.SurveyedLocale;
 import org.akvo.flow.util.ConstantUtil;
-
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.maps.android.clustering.Cluster;
-import com.google.maps.android.clustering.ClusterManager;
-import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import org.osmdroid.util.ResourceProxyImpl;
+import org.osmdroid.views.MapView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapFragment extends SupportMapFragment implements LoaderCallbacks<Cursor>, OnInfoWindowClickListener {
+public class MapFragment extends Fragment implements LoaderCallbacks<Cursor> {
     private static final String TAG = MapFragment.class.getSimpleName();
 
     private long mSurveyGroupId;
@@ -68,8 +58,7 @@ public class MapFragment extends SupportMapFragment implements LoaderCallbacks<C
 
     private boolean mSingleRecord = false;
 
-    private GoogleMap mMap;
-    private ClusterManager<SurveyedLocale> mClusterManager;
+    private MapView mMapView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -104,47 +93,6 @@ public class MapFragment extends SupportMapFragment implements LoaderCallbacks<C
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mDatabase = new SurveyDbAdapter(getActivity());
-        if (mMap == null) {
-            mMap = getMap();
-            configMap();
-        }
-    }
-
-    private void configMap() {
-        if (mMap != null) {
-            mMap.setMyLocationEnabled(true);
-            mMap.setOnInfoWindowClickListener(this);
-            mClusterManager = new ClusterManager<SurveyedLocale>(getActivity(), mMap);
-            mClusterManager.setRenderer(new PointRenderer());
-            mMap.setOnMarkerClickListener(mClusterManager);
-            mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
-                @Override
-                public void onCameraChange(CameraPosition cameraPosition) {
-                    cluster();
-                }
-            });
-            centerMap(null);
-        }
-    }
-
-    private void cluster() {
-        if (mMap == null) {
-            return;
-        }
-
-        final LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
-        LatLng ne = bounds.northeast, sw = bounds.southwest;
-        double latDst = Math.abs(ne.latitude - sw.latitude);
-        double lonDst = Math.abs(ne.longitude - sw.longitude);
-
-        final double scale = 1d;
-        LatLngBounds newBounds = bounds
-                .including(new LatLng(ne.latitude + latDst/scale, ne.longitude + lonDst/scale))
-                .including(new LatLng(sw.latitude - latDst/scale, ne.longitude + lonDst/scale))
-                .including(new LatLng(sw.latitude - latDst/scale, sw.longitude - lonDst/scale))
-                .including(new LatLng(ne.latitude + latDst/scale, sw.longitude - lonDst/scale));
-
-        new DynamicallyAddMarkerTask().execute(newBounds);
     }
 
     /**
@@ -154,33 +102,11 @@ public class MapFragment extends SupportMapFragment implements LoaderCallbacks<C
      * @param record
      */
     private void centerMap(SurveyedLocale record) {
-        if (mMap == null) {
+        if (mMapView == null) {
             return; // Not ready yet
         }
 
-        LatLng position = null;
-
-        if (record != null && record.getLatitude() != null && record.getLongitude() != null) {
-            // Center the map in the data point
-            position = new LatLng(record.getLatitude(), record.getLongitude());
-        } else {
-            // When multiple points are shown, center the map in user's location
-            LocationManager manager = (LocationManager) getActivity()
-                    .getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            criteria.setAccuracy(Criteria.ACCURACY_FINE);
-            String provider = manager.getBestProvider(criteria, true);
-            if (provider != null) {
-                Location location = manager.getLastKnownLocation(provider);
-                if (location != null) {
-                    position = new LatLng(location.getLatitude(), location.getLongitude());
-                }
-            }
-        }
-
-        if (position != null) {
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 10));
-        }
+        // TODO
     }
 
     @Override
@@ -202,14 +128,11 @@ public class MapFragment extends SupportMapFragment implements LoaderCallbacks<C
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-        View mapView = super.onCreateView(inflater, container, savedInstanceState);
-
-        View v = inflater.inflate(R.layout.map_fragment, container, false);
-        FrameLayout layout = (FrameLayout) v.findViewById(R.id.map_container);
-
-        layout.addView(mapView, 0);
-
-        return v;
+        ResourceProxyImpl resourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+        mMapView = new MapView(inflater.getContext(), 256, resourceProxy);
+        mMapView.setBuiltInZoomControls(true);
+        mMapView.setMultiTouchControls(true);
+        return mMapView;
     }
 
     /**
@@ -221,28 +144,15 @@ public class MapFragment extends SupportMapFragment implements LoaderCallbacks<C
             if (mSingleRecord) {
                 // Just get it from the DB
                 SurveyedLocale record = mDatabase.getSurveyedLocale(mRecordId);
-                if (mMap != null && record != null && record.getLatitude() != null
+                if (mMapView != null && record != null && record.getLatitude() != null
                         && record.getLongitude() != null) {
-                    mMap.clear();
-                    mMap.addMarker(new MarkerOptions()
-                            .position(new LatLng(record.getLatitude(), record.getLongitude()))
-                            .title(record.getDisplayName(getActivity()))
-                            .snippet(record.getId()));
+                    // TODO
                     centerMap(record);
                 }
             } else {
                 getLoaderManager().restartLoader(0, null, this);
             }
         }
-    }
-
-    @Override
-    public void onInfoWindowClick(Marker marker) {
-        if (mSingleRecord) {
-            return; // Do nothing. We are already inside the record Activity
-        }
-        final String surveyedLocaleId = marker.getSnippet();
-        mListener.onRecordSelected(surveyedLocaleId);
     }
 
     // ==================================== //
@@ -271,60 +181,10 @@ public class MapFragment extends SupportMapFragment implements LoaderCallbacks<C
             } while (cursor.moveToNext());
         }
         cursor.close();
-        cluster();
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-    }
-
-    /**
-     * This custom renderer overrides original 'bucketed' names, in order to display the accurate
-     * number of markers within a cluster.
-     */
-    class PointRenderer extends DefaultClusterRenderer<SurveyedLocale> {
-
-        public PointRenderer() {
-            super(getActivity(), getMap(), mClusterManager);
-        }
-
-        @Override
-        protected void onBeforeClusterItemRendered(SurveyedLocale item, MarkerOptions markerOptions) {
-            markerOptions
-                    .title(item.getDisplayName(getActivity()))
-                    .snippet(item.getId());
-            super.onBeforeClusterItemRendered(item, markerOptions);
-        }
-
-        @Override
-        protected int getBucket(Cluster<SurveyedLocale> cluster) {
-            return cluster.getSize();
-        }
-
-        @Override
-        protected String getClusterText(int bucket) {
-            return String.valueOf(bucket);
-        }
-
-    }
-
-    private class DynamicallyAddMarkerTask extends AsyncTask<LatLngBounds, Void, Void> {
-
-        @Override
-        protected Void doInBackground(LatLngBounds... bounds) {
-            mClusterManager.clearItems();
-            for (SurveyedLocale item : mItems) {
-                if (item.getPosition() != null && bounds[0].contains(item.getPosition())) {
-                    mClusterManager.addItem(item);
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            mClusterManager.cluster();
-        }
     }
 
 }
