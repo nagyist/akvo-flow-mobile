5ce83ba0faea6e91fe613ab0507ba50ae2fc6131
rumca <ruarcc@gmail.com>
2015-01-21 13:04:02 +0000

    [#250] adding basic starting point for FLOW & RSR apk tests

--- test/appium/src/test/java/org/akvo/appium/FLOWTests.java
+++ test/appium/src/test/java/org/akvo/appium/FLOWTests.java
@@ -1,8 +1,43 @@
 package org.akvo.appium;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.URL;
+
 /**
  * Author: Ruarcc McAloon
  * Date: 20/01/15
  */
 public class FLOWTests {
+
+    private AppiumDriver driver;
+
+    private static final String FLOW_APP = "flow-2.1.0.1.apk";
+
+    @Before
+    public void setUp() throws Exception {
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "../apps/");
+        File app = new File(appDir, FLOW_APP);
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+        capabilities.setCapability("deviceName","6435b877");
+        capabilities.setCapability("platformVersion", "4.4.2");
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("app", app.getAbsolutePath());
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
 }
--- test/appium/src/test/java/org/akvo/appium/FieldSurveyTests.java
+++ test/appium/src/test/java/org/akvo/appium/FieldSurveyTests.java
@@ -2,12 +2,14 @@ package org.akvo.appium;
 
 import static org.junit.Assert.assertEquals;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 
 import java.io.File;
 import java.net.URL;
 import java.util.List;
 
+import io.appium.java_client.remote.MobileBrowserType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,33 +24,46 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  */
 public class FieldSurveyTests {
 
+//    TODO: Eventually remove these locator strategy reminders
+//    driver.findElement(MobileBy.className("UIAButton"));
+//    driver.findElement(MobileBy.xpath("//android.widget.EditText"));
+//    driver.findElement(MobileBy.id("com.aut.android:id/searchButton"));
+//    driver.findElement(MobileBy.AccessibilityId("Sign In"));
+//    driver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().className('android.widget.TextView')")) ;
+
     private AppiumDriver driver;
+    private static final String FIELD_SURVEY_APP = "fieldsurvey-1.13.6.apk";
 
     @Before
     public void setUp() throws Exception {
         File classpathRoot = new File(System.getProperty("user.dir"));
         File appDir = new File(classpathRoot, "../apps/");
-        File app = new File(appDir, "fieldsurvey-1.13.6.apk");
+        File app = new File(appDir, FIELD_SURVEY_APP);
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
         capabilities.setCapability("deviceName","6435b877");
-        capabilities.setCapability("platformVersion", "4.4");
+        capabilities.setCapability("platformVersion", "4.4.2");
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("app", app.getAbsolutePath());
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
-    @After
-    public void tearDown() throws Exception {
-        driver.quit();
+    @Test
+    public void testActionDisallowedWithoutUserSelected(){
+        WebElement el = driver.findElement(By.id("com.gallatinsystems.survey.device:id/buttonText"));
+        assertEquals("Manage Users", el.getText());
     }
 
     @Test
-    public void actionWithoutUserSelection(){
-          WebElement el = driver.findElement(By.id("com.gallatinsystems.survey.device:id/buttonText"));
-          assertEquals("Manage Users", el.getText());
+    public void testAddUserAndSelect(){
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
     }
 
 }
